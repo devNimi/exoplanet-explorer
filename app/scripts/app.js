2db270b3c313f4a004b76f81dfb5167f6af6d9bf
Nimesh Jain <pancholinimesh25@gmail.com>
2018-05-30 19:44:39 +0530

    .map implementation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -59,6 +59,11 @@ Instructions:
     Your code goes here! Uncomment the next line when you're ready to start!
      */
 
-    // getJSON('../data/earth-like-results.json')
+    getJSON('../data/earth-like-results.json').then(response=>{
+      response.results.map((result) =>{
+        getJSON(result).then(createPlanetThumb)
+      })
+
+    })
   });
 })(document);
